b029e9b21b07f28937cbbe39ef1f0d152fa9d1c0
Tinty <dualdng@gmail.com>
2014-09-09 18:02:00 +0800

    add the soft

--- soft/js/main.js
+++ soft/js/main.js
@@ -9,10 +9,13 @@ $(document).ready(function()
 		}
 )
 $(document).on('click','a#list',function(){
+//		var audio=document.getElementById('music');
+		var audio=$('#music')[0];
 		var songid=$(this).attr('val');
 		var url='../../music/'+songid+'.mp3';
 		$('#music').attr('src',url);
 		$('#music').attr('val',songid);
+		audio.load();//下标0的作用，推测为当前页面有多个audio的时候，选择第几个(the foot '0' means that when there are more than one audio tags ,you should select the element by the foot)
 		play();
 		return false;
 
@@ -20,28 +23,46 @@ $(document).on('click','a#list',function(){
 )
 function play()
 {
-		$('#music')[0].play();//下标0的作用，推测为当前页面有多个audio的时候，选择第几个(the foot '0' means that when there are more than one audio tags ,you should select the element by the foot)
-		setTimeout(duration(),50000);
+//		var audio=document.getElementById('music');
+		var audio=$('#music')[0];
+		audio.play();//下标0的作用，推测为当前页面有多个audio的时候，选择第几个(the foot '0' means that when there are more than one audio tags ,you should select the element by the foot)
+		duration();
 }
 function pause()
 {
+//		var audio=document.getElementById('music');
+		var audio=$('#music')[0];
 		//js method
 //		var audio=document.getElementById('music');
 //		audio.pause();
 //jquery method
-		$('#music')[0].pause();
+		audio.pause();
 }
 function duration()
 {
 		clearInterval(a);
-		var length=$('#music')[0].duration;
+		/** javascript 的写法
+		var audio=document.getElementById('music');
+		audio.addEventListener('loadedmetadata', function() {
+				var length=audio.duration;
 		var time=length/60;
 		$('#time').text(time);
+    });
+ **/
+		/** jquery 写法**/
+		var audio=$('#music')[0];
+		$('#music').on('loadedmetadata', function() {
+				var length_full=audio.duration;
+				var time_full=time_transfer(length_full);
+				$('#time').text(time_full);
+		});
+
 		var a=setInterval(function(){
 //		var length_2=$('#music')[0].played.end(0);
-		var time_pass=$('#music')[0].currentTime;
+		var length_pass=audio.currentTime;
+		var time_pass=time_transfer(length_pass);
 		$('#time_p').text(time_pass);
-		var width=100/time_pass;
+		var width=(length_pass/length_full)*100;
 		$('#scroll').css({'width':width});
 		},1000);
 }
@@ -65,3 +86,14 @@ function pre()
 		$('#music').attr('val',songid);
 		play();
 }
+function time_transfer(length) //s时间格式转换，由秒转换为分+秒
+{
+		var time=(length/60).toFixed(2);
+		var time_str=time.toString();
+		var time_array=time_str.split('.');
+		var time_m=time_array[0];
+		var time_s=Math.round((time_array[1]/100)*60);
+		var res=time_m+'分'+time_s+'秒';
+		return  res;
+}
+
