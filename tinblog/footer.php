4b4dbd48dfb88493cc78be8a41bea26d8114ab0e
Tinty <dualdng@gmail.com>
2014-09-22 18:16:08 +0800

    add the navibar_two

--- tinblog/footer.php
+++ tinblog/footer.php
@@ -2,6 +2,7 @@
 <nav id='nav_foot'>
 <span><a href='http://www.uuuuj.com/about'>About</a>
 <a href='http://www.uuuuj.com/archive.php'>Archive</a>
+<a href='http://www.uuuuj.com/links.php'>Links</a>
 </span><br />
 <span>ICP备88888888号</span>
 <div class='foot'>Music <a href='javascript:void(0);'  onclick="document.getElementById('backmusic').play()" >ON&nbsp|</a><a href='javascript:void(0);' onclick="document.getElementById('backmusic').pause()" >&nbspOFF</a><audio id='backmusic'src='#'  loop='loop'></audio>
