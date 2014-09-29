a63df2bf057f1e7b18cc27e3d28c95bccc938b33
Tinty <dualdng@gmail.com>
2014-09-29 19:06:19 +0800

    the whole thing

--- tinblog/category_page.php
+++ tinblog/category_page.php
@@ -6,8 +6,6 @@ include('header.php');
 <article class="main_content">
 <h2>CATAGORY:<?php echo $category;?></h2>
 <div id='article'><?php cate_list($category);?></div>
-<div id='side'><?php include('side.php');?></div>
-<div style='clear:both'></div>
 <div id='pagenavi'><?php pagenavi_cate($category);?></div>
 </article>
 <?php include('footer.php');?>
